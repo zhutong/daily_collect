532daaae6f71068ec61a8bce3401d1062c1049b0
Zhu Tong <zhutong9116@126.com>
2019-05-02 13:28:35 +0800

    Update dev.py

--- others/dev.py
+++ others/dev.py
@@ -17,12 +17,12 @@ FE_FN = os.path.join(PATH, 'n7k_mac_fe.json')
 
 
 re_BD = re.compile('\d+\s+(?P<vlan>\d+)\s+(?P<bd>\d+)')
-re_MAC = re.compile('\w?\s+?(?P<vlan>\d+)\s+'
-                    '(?P<mac>\w+\.\w+\.\w+)\s+dynamic\s+.*', re.I)
 re_MAC_PORT = re.compile('\w?\s+?(?P<vlan>\d+)\s+'
-                    '(?P<mac>\w+\.\w+\.\w+)\s+dynamic\s+.*', re.I)
+                         '(?P<mac>\w+\.\w+\.\w+)\s+dynamic\s+'
+                         '(\d+\s+F\s+F\s+|ip.*?\s+|\s)'
+                         '(?P<port>\S+)', re.I)
 re_FE_MAC = re.compile('(?P<fe>\d+)\s+\d+\s+\d+\s+(?P<bd>\d+)'
-                     '\s+(?P<mac>\w+\.\w+\.\w+)\s+', re.I)
+                       '\s+(?P<mac>\w+\.\w+\.\w+)\s+', re.I)
 
 
 GW_MAC_PREFIXES = '0000.0c07', '0001.d7', '000a.49', '0023.e9', 'f415.63'
@@ -37,10 +37,10 @@ def __parse_bd(text):
 
 
 def __parse_mac(text):
-    mac_dict = defaultdict(set)
-    for m in re_MAC.finditer(text):
-        vlan, mac = m.groups()
-        mac_dict[vlan].add(mac)
+    mac_dict = defaultdict(list)
+    for m in re_MAC_PORT.finditer(text):
+        vlan, mac, _, port = m.groups()
+        mac_dict[vlan].append((mac, port))
     return mac_dict
 
 
@@ -58,6 +58,14 @@ def __parse_fe_mac(hostname, text, bd_dict):
         else:
             slot_mac_dict[fe][vlan] = [mac]
     return slot_mac_dict
+
+
+def __mac_tuple2mac_dict(mac_tuple):
+    mac_dict = defaultdict(dict)
+    for vlan, mac_ports in mac_tuple.items():
+        for mac, port in mac_ports:
+            mac_dict[vlan][mac] = port
+    return mac_dict
 
 
 def check_one(n7k_module_chips, hostname, fn):
@@ -80,16 +88,16 @@ def check_one(n7k_module_chips, hostname, fn):
                 slot_fe_mac_dict = __parse_fe_mac(hostname, txt, bd_dict)
                 fe_bd_mac_dict[cmd.split()[-1]] = slot_fe_mac_dict
 
-        hw_missed_mac = defaultdict(list)
-        sw_missed_mac = defaultdict(list)
         # check slot software mac entry
+        hw_missed_mac = defaultdict(list)
         slot_vlan_chips = n7k_module_chips[hostname]
         for vlan, slot_dict in slot_vlan_chips.items():
             vlan_macs = global_mac_dict[vlan]
-            # vlan_mac_set = set(vlan_macs)
-            vlan_gw_mac_set = set(
-                [m for m in vlan_macs if m.startswith(GW_MAC_PREFIXES)])
-            if not vlan_gw_mac_set:
+            # gen_vlan_mac = (m[0] for m in vlan_macs)
+            gen_vlan_mac = (m[0]
+                            for m in vlan_macs if m[0].startswith(GW_MAC_PREFIXES))
+            vlan_mac_set = set(gen_vlan_mac)
+            if not vlan_mac_set:
                 # logging.warn('%s no GW mac for vlan: %s', hostname, vlan)
                 continue
             for slot, fes in slot_dict.items():
@@ -97,22 +105,29 @@ def check_one(n7k_module_chips, hostname, fn):
                     fe = str(fe)
                     chip_vlan_mac_set = set(
                         fe_bd_mac_dict[slot][fe].get(vlan, []))
-                    # missed = vlan_mac_set - chip_vlan_macs
-                    missed = vlan_gw_mac_set - chip_vlan_mac_set
+                    missed = vlan_mac_set - chip_vlan_mac_set
                     for m in missed:
                         key = '%s@%s' % (m, vlan)
                         hw_missed_mac[key].append('%s/%s' % (slot, fe))
+
         # check slot software mac entry
+        sw_errored_mac = defaultdict(list)
+        global_vlan_mac_dict = __mac_tuple2mac_dict(global_mac_dict)
         for slot, slot_dict in slot_sw_mac_dict.items():
-            for vlan, sw_mac_dict in slot_dict.items():
-                missed =  global_mac_dict[vlan] - sw_mac_dict
-                for m in missed:
-                    key = '%s@%s' % (m, vlan)
-                    sw_missed_mac[key].append(slot)        
+            slot_vlan_mac_dict = __mac_tuple2mac_dict(slot_dict)
+            for vlan, g_mac_dict in global_vlan_mac_dict.items():
+                s_mac_dict = slot_vlan_mac_dict[vlan]
+                for m, p in g_mac_dict.items():
+                    s_p = s_mac_dict.get(m)
+                    if p=='vPC' or s_p is None:
+                        continue
+                    if s_p != p:
+                        key = '%s@%s' % (m, vlan)
+                        sw_errored_mac[key].append((slot, p, s_p))
     except:
         raise
         logging.warning('Check %s fail', hostname)
-    return hw_missed_mac, sw_missed_mac
+    return hw_missed_mac, sw_errored_mac
 
 
 if __name__ == '__main__':
@@ -126,11 +141,13 @@ if __name__ == '__main__':
             continue
         full_fn = os.path.join(PATH, fn)
         hostname = fn[3:-5]
-        hw_missed_mac, sw_missed_mac = check_one(n7k_module_chips, hostname, full_fn)
+        hw_missed_mac, sw_errored_mac = check_one(
+            n7k_module_chips, hostname, full_fn)
         # if(hw_missed_mac):
         #     logging.error(hostname)
         #     pp(hw_missed_mac)
         #     break
-        if(sw_missed_mac):
+        if(sw_errored_mac):
             logging.error(hostname)
-            pp(sw_missed_mac)
+            pp(sw_errored_mac)
+            # break
